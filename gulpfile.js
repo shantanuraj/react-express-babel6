01e1781f345c73750c971d6b11f54e498633123f
Shantanu <s@sraj.me>
2015-11-10 13:03:16 +0530

    Better gulpfile, with support for watching files, output to public/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,73 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var streamify = require('gulp-streamify');
 var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
-    return browserify({ entries: './src/app.jsx', extensions: ['.jsx'], debug: true })
-        .transform('babelify', {presets: ['es2015', 'react']})
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('public/build'));
+var production = process.env.NODE_ENV === 'production';
+
+var dependencies = [
+  'react',
+  'react-dom'
+];
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile third-party dependencies separately for faster performance.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-vendor', function() {
+  return browserify()
+    .require(dependencies)
+    .bundle()
+    .pipe(source('vendor.bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
 });
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify', ['browserify-vendor'], function() {
+  return browserify('./src/app.jsx')
+    .external(dependencies)
+    .transform('babelify', {presets: ['es2015', 'react']})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('public/js'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Same as browserify task, but will also watch for changes and re-compile.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-watch', ['browserify-vendor'], function() {
+  var bundler = watchify(browserify('./src/app.jsx', watchify.args));
+  bundler.external(dependencies);
+  bundler.transform('babelify', {presets: ['es2015', 'react']});
+  bundler.on('update', rebundle);
+  return rebundle();
+
+  function rebundle() {
+    var start = Date.now();
+    return bundler.bundle()
+      .on('error', function(err) {
+        gutil.log(gutil.colors.red(err.toString()));
+      })
+      .on('end', function() {
+        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+      })
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('public/js'));
+  }
+});
+
+gulp.task('default', ['browserify-watch']);
